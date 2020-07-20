02fec61b0849455bfabbe463c029e8b786657f78
papjco <papjco@gmail.com>
2020-07-20 17:15:12 -0400

    Updated app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Sign In</p>
       </header>
+      <body className="App-body">
+      <form>
+        <p>Username:</p>
+        <input></input>
+        <p>Password:</p>
+        <input></input>
+        <br/>
+        <br/>
+        <button>Log In</button>
+        <br/>
+        <br/>
+        <button>Sign Up</button>
+        </form>
+      </body>
     </div>
   );
 }
