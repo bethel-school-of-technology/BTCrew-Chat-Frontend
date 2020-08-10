0140ab7416ad7ce20ce9243b3163680442542104
Marion <thomasm1124@outlook.com>
2020-08-10 01:38:34 -0400

    login.js

--- src/components/login.js
+++ src/components/login.js
@@ -1,27 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from 'axios'
+import {withRouter} from 'react-router'
 
-export default class Login extends React.Component{
-  render() {
+const Login = withRouter (({ history }) => {
+
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const url = "http://localhost:3000/users"
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		console.log(username, password);
+		let user = {
+			username: username,
+			password: password
+	}
+		let response = await axios.post(`${url}/login`, user);
+
+		console.log(response.data);
+		if(response.data.status === 200) {
+			localStorage.setItem('BSTtoken', response.data.jwt)
+			window.alert(response.data.message);
+            history.push('/profile');
+            
+        
+	}else{
+		window.alert(response.data.message);
+		history.push('/')
+    }   
+
+};
+
     return (
-        <div className="wrapper">
-            <form onSubmit={this.login}>
-                
-                <div className="form-group">
-                    <input type="text" className="form-control" 
-                    onChange={this.handleFormChange}
-                    placeholder="Enter UserName" />
-                </div>
-                <div className="form-group">
-                <input type="password" className="form-control" 
-                onChange={this.handleFormChange}
-                placeholder="Enter Password"/>
-                </div>
-
-                <div className="form-group">
-                    <input type="submit" value="Submit Login" className="btn btn-success btn-block" />
-                </div>
-            </form>
+        <div className="container max-w-full mx-auto md:py-24 px-6">
+        <div className="text-center text-2x1">LOGIN</div>
+        <div className="w-full relative">
+        <div className="md:mt-6">
+        <div className="mt-8" onSubmit={handleSubmit}>
+        <div className="mx-auto max-w-lg">
+            <div className="py-1">
+                <span className="px-1 text-sm text-gray-600">UserName</span>
+                <input placeholder="Enter Username" type="text" value={username}  
+                onChange={e =>setUsername(e.target.value)} />
+                <span className="px-1 text-sm text-gray-600">UserName</span>               
+                <input placeholder="Enter Password" type="text" value={password}  
+                onChange={e =>setPassword(e.target.value)}
+                />
         </div>
-    )
+        </div>
+        </div>
+        </div>
+        </div>
+         </div>
+    ); 
 }
 }
+
+export default Login;
